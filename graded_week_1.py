e0a481eb25cc2e5cce0ff2b42dba6ca4681cdf0f
Amar Sansil <72847504+amarsansil@users.noreply.github.com>
2023-10-09 23:37:49 +0530

    Update graded_week_1.py

--- graded_week_1.py
+++ graded_week_1.py
@@ -46,11 +46,13 @@ for i in lines:
 # Challenge: Create a list called emotions that contains the first word of every line in emotion_words.txt.
 
 emotions = []
-with open ("emotion_words.txt","r") as obj:
-    
-    for i in lines:
-        a = i.split()
-        emotions.append(a[0])  
+
+with open("emotion_words.txt", "r") as f:
+    for line in f:
+        emotions.append(line.split()[0])
+
+print(emotions)
+
         
         
         
